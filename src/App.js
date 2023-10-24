1be73f5c8fc0cc40d0f0671063246a7c27572f21
2021 - Bahrul Ulum 261 <111725185+FullStackFlair@users.noreply.github.com>
2023-10-24 15:23:10 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from 'react';
+import { BrowserRouter as Router, Route , Switch} from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Menu from './pages/Menu';
+import AboutUs from './pages/AboutUs';
+import Contact from './pages/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={Menu} />
+        <Route path="/about" component={AboutUs} />
+        <Route path="/contact" component={Contact} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/AboutUs.js
@@ -0,0 +1,8 @@
+import React from "react";
+import Menu from "./Menu";
+
+function AboutUs(){
+    return <div>About Us</div>
+}
+
+export default Menu;
